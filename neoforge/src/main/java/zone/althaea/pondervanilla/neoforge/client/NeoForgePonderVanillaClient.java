1afcd13f99374706b60f7296ba7aef2b5efcfd35
marshmallow <git@althaea.zone>
2026-05-19 21:10:59 +1000

    move ponder lib to toml

--- neoforge/src/main/java/zone/althaea/pondervanilla/neoforge/client/NeoForgePonderVanillaClient.java
+++ neoforge/src/main/java/zone/althaea/pondervanilla/neoforge/client/NeoForgePonderVanillaClient.java
@@ -10,7 +10,6 @@ import zone.althaea.pondervanilla.client.PonderVanillaClient;
 
 @Mod(value = PonderVanilla.MOD_ID, dist = Dist.CLIENT)
 public class NeoForgePonderVanillaClient {
-
     public NeoForgePonderVanillaClient(IEventBus modEventBus) {
         final var context = new NeoForgeLoadContext(modEventBus);
         BalmClient.initializeMod(PonderVanilla.MOD_ID, context, PonderVanillaClient::initialize);
